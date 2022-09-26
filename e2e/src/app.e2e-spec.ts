9c30eb651e32af0ffc30a4580abefa1a4889ae02
qinzhihao-conghua <1043419697@qq.com>
2022-09-26 18:01:53 +0800

    重命名

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('bpmn-test app is running!');
+    expect(page.getTitleText()).toEqual('ng-bpmn app is running!');
   });
 
   afterEach(async () => {
